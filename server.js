1f9ac00897ee3f3721574ca0c03b767c1db7e03c
Dustin Irving <dustin.irving@gmail.com>
2023-11-11 15:04:56 -0500

    feat: setup basic node express server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+const port = 3000; // default port to listen
+
+app.get('/', (req, res) => {
+  res.send('Hello World with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
