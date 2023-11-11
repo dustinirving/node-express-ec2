f0a9a3408bb8b46e4fdf25ca1571005b768c9d24
Dustin Irving <dustin.irving@gmail.com>
2023-11-11 18:37:32 -0500

    change port 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express();
-const port = 5001; // default port to listen
+const port = 3000; // default port to listen
 
 app.get('/', (req, res) => {
   res.send('Hello World with Express!');
